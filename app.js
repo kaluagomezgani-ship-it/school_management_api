664b3f963ee518873f8985751a618f512ee5f399
kaluagomezgani-ship-it <kaluagomezgani@gmail.com>
2026-07-06 18:52:20 +0200

    added routes and connected to the database

---
+++ app.js
@@ -0,0 +1,18 @@
+//IMPORTS
+const paths=require("./ROUTES/routes");
+require("dotenv").config();
+const express=require("express");
+const app=express();
+
+app.use(express.json());
+
+app.use("/school",paths);
+// app.get("/",(rep,res)=>{
+//     res.status(200).json({"message":"successful"})
+// })
+
+app.listen("3000", ()=>{
+    console.log("server is running")
+})
+
+// module.exports=app;
